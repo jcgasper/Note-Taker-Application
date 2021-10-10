1bb023e1d764570805e08fe0dab1e3e4e70d432a
Jacob Gasper <jacobgasper@gmail.com>
2021-10-10 17:49:38 -0400

    push heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -46,6 +46,7 @@ app.post(path, function(req,res) {
         });
       });
       res.send("note submitted");
+      
 
 
 });
@@ -53,7 +54,34 @@ app.post(path, function(req,res) {
 
 
     //API DELETE REQUEST
-
-
-
+    app.delete('/api/notes/:id', function(req, res) {
+        // Gets id number of note to delete
+        let deleteNote = req.params.id;
+        
+    
+        fs.readFile('./db/db.json', (err, data) => {
+          if (err) 
+          throw err;
+    
+          
+          dbData = JSON.parse(data);
+          
+          // 
+          
+          for (let i = 0; i < dbData.length; i++) {
+            if (dbData[i].id === Number(deleteNote)) {
+              dbData.splice([i], 1);
+            }
+          }
+          console.log(dbData);
+          stringData = JSON.stringify(dbData);
+    
+          fs.writeFile('./db/db.json', stringData, (err, data) => {
+            if (err) 
+            throw err;
+          });
+        });
+        // 
+        res.send("Note Deleted");
+      });
 };
